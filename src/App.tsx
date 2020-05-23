e3d2930cce90583f61c296d33381c73c614c3e73
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-05-23 20:37:47 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import "./App.css";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      margin: theme.spacing(1),
+      width: "50ch",
+    },
+  }),
+);
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar></Toolbar>
+      </AppBar>
+
+      <form>
+        <TextField className={classes.text} label="タスクを入力してください" />
+      </form>
+
+      <List>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value}>
+              <ListItemIcon>
+                <Checkbox />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary="テスト" />
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 }
